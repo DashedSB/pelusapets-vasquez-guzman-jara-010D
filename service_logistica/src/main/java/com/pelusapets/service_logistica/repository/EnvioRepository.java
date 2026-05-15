20037db4edc9fe3a7c97a46a01c04e3b0158af38
Bryan Guzman <bry.guzman@duocuc.cl>
2026-05-15 17:19:44 -0400

    subida repositorio logistica

--- service_logistica/src/main/java/com/pelusapets/service_logistica/repository/EnvioRepository.java
+++ service_logistica/src/main/java/com/pelusapets/service_logistica/repository/EnvioRepository.java
@@ -1,9 +1,34 @@
 package com.pelusapets.service_logistica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.pelusapets.service_logistica.model.Envio;
 
 @Repository
 public interface EnvioRepository extends JpaRepository<Envio, Long> {
+
+    
+    List<Envio> findByEstado(String estado);
+
+    
+    @Query("""
+        SELECT e.estado AS estado,
+        COUNT(e) AS cantidad
+        FROM Envio e
+        GROUP BY e.estado
+        """)
+    List<Object[]> conteoPorEstado();
+
+    
+    @Query("""
+        SELECT e FROM Envio e
+        JOIN FETCH e.proveedor p
+        WHERE p.nombre = :nombreProveedor
+        """)
+    List<Envio> findEnvioPorNombreProveedor(@Param("nombreProveedor") String nombreProveedor);
 }
--- service_logistica/src/main/java/com/pelusapets/service_logistica/repository/ProveedorRepository.java
+++ service_logistica/src/main/java/com/pelusapets/service_logistica/repository/ProveedorRepository.java
@@ -1,9 +1,24 @@
 package com.pelusapets.service_logistica.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.pelusapets.service_logistica.model.Proveedor;
 
 @Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+
+   
+    Proveedor findByCorreo(String correo); 
+
+    
+    @Query("""
+        SELECT p FROM Proveedor p
+        WHERE p.activo = :activo
+        """)
+    List<Proveedor> findProveedoresPorEstado(@Param("activo") boolean activo);
 }
